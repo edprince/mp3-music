795786b02cfecacc1a309b6c2db61b7aaf153f75
Ed Prince <edward_prince@hotmail.com>
2018-11-16 13:20:21 +0000

    added mongo, but not created db

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -12,13 +12,15 @@ afterAll(async () => {
 
 describe('routing tests', () => {
   test('get home route GET /', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     const response = await request(server).get('/');
     expect(response.status).toEqual(200);
+    expect(response.type).toEqual("application/json");
   });
   test('get playlist route GET /playlists', async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     const response = await request(server).get('/playlists');
     expect(response.status).toEqual(200);
+    expect(response.type).toEqual("application/json");
   });
 });
